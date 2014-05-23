4959cf8f74a64348779dc6ed4d475c6c49e938c0
Premkumar Masilamani <prem82@gmail.com>
2014-05-23 17:07:43 +0530

    added the navigation to enhance the silo structure

--- _backup/popular-posts/getdata.sh
+++ _backup/popular-posts/getdata.sh
@@ -1,2 +1,9 @@
-javac GAData.java
-java GAData
+
+for line in books health social personal self-improvement challenges technology travelogue thought-experiments science
+do
+	grep -Ril "$line" /home/smileprem/git/prem82.github.io/_posts/*.md | cut -c56-| rev | cut -c4- | rev > temp/"$line".txt
+done
+
+
+#javac GAData.java
+#java GAData
